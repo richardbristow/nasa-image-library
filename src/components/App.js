281db4e6d7aab478704552a0b472986751d85865
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2017-09-11 21:45:06 +0100

    Added SearchBar component
    Added SearchBar component.
    Has checkboxes to allow the selection of the media type.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/css/App.css';
 
+import SearchBar from './SearchBar';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Nasa Image Library</h2>
+          <SearchBar onSearch={this.handleSearch} />
         </div>
       </div>
     );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: '',
+      image: true,
+      video: true,
+      audio: true,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="searchTerm"
+          type="text"
+          value={this.state.searchTerm}
+          onChange={this.handleInputChange}
+          placeholder="e.g. Jupiter"
+        />
+        <label htmlFor="imageCheckbox">Images</label>
+        <input
+          name="image"
+          type="checkbox"
+          id="imageCheckbox"
+          checked={this.state.image}
+          onChange={this.handleInputChange}
+        />
+        <label htmlFor="videoCheckbox">Videos</label>
+        <input
+          name="video"
+          type="checkbox"
+          id="videoCheckbox"
+          checked={this.state.video}
+          onChange={this.handleInputChange}
+        />
+        <label htmlFor="audioCheckbox">Audio</label>
+        <input
+          name="audio"
+          type="checkbox"
+          id="audioCheckbox"
+          checked={this.state.audio}
+          onChange={this.handleInputChange}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
