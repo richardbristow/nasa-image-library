046c07f272823cfe728005718a19abf7ae6753e9
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2017-09-12 20:46:39 +0100

    Added GalleryItem component
    GalleryItem renders the results retrieved from the api.
    They can either be images, videos, or audio.

---
+++ src/components/GalleryItem.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function GalleryItem(props) {
+  const itemData = props.imageData.data[0];
+  if (itemData.media_type === 'image') {
+    return (
+      <div>
+        <img
+          data-media-type="image"
+          data-media-title={itemData.title}
+          data-media-desc={itemData.description}
+          data-nasa_id={itemData.nasa_id}
+          src={props.imageData.links[0].href}
+          alt={itemData.title}
+        />
+      </div>
+    );
+  } else if (itemData.media_type === 'video') {
+    return (
+      <div>
+        <img
+          data-media-type="video"
+          data-media-title={itemData.title}
+          data-media-desc={itemData.description}
+          data-nasa_id={itemData.nasa_id}
+          src="http://via.placeholder.com/250x150"
+          alt={itemData.title}
+        />
+      </div>
+    );
+  } else if (itemData.media_type === 'audio') {
+    return (
+      <div>
+        <img
+          data-media-type="audio"
+          data-media-title={itemData.title}
+          data-media-desc={itemData.description}
+          data-nasa_id={itemData.nasa_id}
+          src="http://via.placeholder.com/250x150"
+          alt={itemData.title}
+        />
+      </div>
+    );
+  }
+
+  return null;
+}
+
+export default GalleryItem;
