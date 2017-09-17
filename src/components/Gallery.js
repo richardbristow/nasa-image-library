54c1c08a006061ae9eae6214610d95afb2b24ebd
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2017-09-17 21:47:49 +0100

    All media types now in gallery modal
    All media types now can be displayed in the gallery modal.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,45 +8,84 @@ import GalleryModal from './GalleryModal';
 class Gallery extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { modalOpen: false };
+    this.state = {
+      modalOpen: false,
+      modalDataObj: null,
+      imagesLoading: true,
+    };
 
     this.renderGalleryItems = this.renderGalleryItems.bind(this);
     this.handlePageChange = this.handlePageChange.bind(this);
     this.renderPageNavigation = this.renderPageNavigation.bind(this);
     this.closeGalleryModal = this.closeGalleryModal.bind(this);
     this.openGalleryModal = this.openGalleryModal.bind(this);
+    this.handleImagesLoaded = this.handleImagesLoaded.bind(this);
   }
 
+
+  // Closes the modal
   closeGalleryModal() {
     this.setState({
       modalOpen: false,
+      modalDataObj: null,
     });
   }
 
+
+  // Handles the click event for the previous and next buttons
   handlePageChange(event) {
-    const direction = (event.target.innerHTML).toLowerCase();
     const pageUrl = event.target.dataset.pageUrl;
     this.props.onGetData(pageUrl);
   }
 
-  openGalleryModal(event) {
+
+  // Querys the dom to check if the images in the gallery have loaded
+  handleImagesLoaded() {
+    const images = this.galleryImages.querySelectorAll('img');
+    let allLoaded = true;
+    for (let i = 0; i < images.length; i++) {
+      if (!images[i].complete) {
+        allLoaded = false;
+      }
+    }
     this.setState({
-      modalOpen: true,
+      imagesLoading: !allLoaded,
     });
   }
 
+
+  // Handles the click event to open a modal
+  openGalleryModal(event) {
+    let itemData = event.target.parentNode.dataset;
+    if (event.target.parentNode.className === 'gallery-items-wrapper') {
+      itemData = event.target.dataset;
+    }
+    this.setState({
+      modalOpen: true,
+      modalDataObj: {
+        modalNasaId: itemData.nasaId,
+        modalTitle: itemData.mediaTitle,
+        modalDescription: itemData.mediaDesc,
+        modalType: itemData.mediaType,
+      },
+    });
+  }
+
+
+  // Loops through the api results and returns gallery items
   renderGalleryItems() {
-    if (Object.prototype.hasOwnProperty.call(this.props.galleryData, 'items')) {
+    if (this.props.galleryData.items) {
       const imageArr = this.props.galleryData.items;
-      const galleryItems = imageArr.map((image) => {
-        return (<GalleryItem
+      const galleryItems = imageArr.map(image =>
+        (<GalleryItem
+          onImagesLoaded={this.handleImagesLoaded}
           key={image.data[0].nasa_id}
           imageData={image}
           openModal={this.openGalleryModal}
-        />);
-      });
+        />),
+      );
       return (
-        <div>
+        <div className="gallery-items-wrapper">
           {galleryItems}
         </div>
       );
@@ -54,6 +93,10 @@ class Gallery extends React.Component {
     return null;
   }
 
+
+  // Creates the next and previous buttons, based on whether there
+  // are more results to display.
+  // Also gets the total results for the search
   renderPageNavigation() {
     const pageNav = {
       next: null,
@@ -61,11 +104,16 @@ class Gallery extends React.Component {
       totalHits: null,
     };
     // Check for the metadata property, and get the total hits
-    if (Object.prototype.hasOwnProperty.call(this.props.galleryData, 'metadata')) {
-      pageNav.totalHits = this.props.galleryData.metadata.total_hits;
+    if (this.props.galleryData.metadata) {
+      pageNav.totalHits = (<div className="page-nav-total-hits">
+        <span className="totalHitsText">Found <span className="totalHitsTextBold">
+          {this.props.galleryData.metadata.total_hits} </span>
+          results.
+        </span>
+      </div>);
     }
     // Check for the links property
-    if (Object.prototype.hasOwnProperty.call(this.props.galleryData, 'links')) {
+    if (this.props.galleryData.links) {
       const linkArr = this.props.galleryData.links;
       // If linkArr > 0 there are buttons to display
       if (linkArr.length > 0) {
@@ -89,16 +137,25 @@ class Gallery extends React.Component {
     return pageNav;
   }
 
+
   render() {
     const pageNav = this.renderPageNavigation();
 
     return (
-      <div className="gallery-wrapper">
-        <GalleryModal isModalOpen={this.state.modalOpen} closeModal={this.closeGalleryModal} />
-        {pageNav.prev !== null ? pageNav.prev : null}
-        {pageNav.next !== null ? pageNav.next : null}
-        {pageNav.totalHits !== null ? pageNav.totalHits : null}
+      <div className="gallery-wrapper" ref={(c) => { this.galleryImages = c; }}>
+        <GalleryModal
+          modalDataObj={this.state.modalDataObj}
+          isModalOpen={this.state.modalOpen}
+          closeModal={this.closeGalleryModal}
+        />
         {this.renderGalleryItems()}
+        <div className="page-navigation-wrapper">
+          <div className="page-navigation-buttons">
+            {pageNav.prev !== null ? pageNav.prev : null}
+            {pageNav.next !== null ? pageNav.next : null}
+          </div>
+          {pageNav.totalHits !== null ? pageNav.totalHits : null}
+        </div>
       </div>
     );
   }
--- src/components/GalleryModal.js
+++ src/components/GalleryModal.js
@@ -1,17 +1,150 @@
 import React from 'react';
 import '../styles/css/GalleryModal.css';
 
-function GalleryModal(props) {
-  if (props.isModalOpen === false) {
-    return null;
+import LoadingSpinner from './LoadingSpinner';
+
+class GalleryModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      assetData: {},
+    };
+
+    this.getModalContent = this.getModalContent.bind(this);
+    this.renderModal = this.renderModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
-  return (
-    <div>
-      <h1>This is the Modal</h1>
-      <button onClick={props.closeModal}>Close</button>
-    </div>
-  );
+
+  // Perfom another api call to get the info for the modal
+  async getModalContent() {
+    const modalData = this.props.modalDataObj;
+    let metaUrl = `https://images-api.nasa.gov/asset/${modalData.modalNasaId}`;
+    metaUrl = metaUrl.replace(/ /g, '%20');
+
+    // fetch the data to display in the modal
+    const response = await fetch(metaUrl);
+    let assetData = await response.json();
+    assetData = assetData.collection;
+
+    const assetObj = {};
+    if (modalData.modalType.toLowerCase() === 'image') {
+      // Get a better quality image
+      assetObj.imageHref = assetData.items[0].href;
+    } else if (modalData.modalType.toLowerCase() === 'video') {
+      // Get the video link and video thumbnail to display
+      assetObj.subsHref = [];
+      const vidThumb = [];
+      assetData.items.forEach((vid) => {
+        if (vid.href.endsWith('orig.mp4')) {
+          assetObj.vidHref = vid.href;
+        } else if (vid.href.endsWith('.png') || vid.href.endsWith('.jpg')) {
+          vidThumb.push(vid.href);
+        } else if (vid.href.endsWith('.srt') || vid.href.endsWith('.vtt')) {
+          assetObj.subsHref.push(vid.href);
+        }
+      });
+      assetObj.vidThumb = vidThumb[Math.floor(vidThumb.length / 2)];
+    } else {
+      // get the audio link
+      assetObj.audioHref = assetData.items.find((aud) => {
+        if (aud.href.endsWith('128k.mp3') || aud.href.endsWith('128k.m4a')) {
+          return aud.href;
+        }
+        return null;
+      });
+    }
+    this.setState({
+      loading: false,
+      assetData: assetObj,
+    });
+  }
+
+
+  // when close button or backdrop is clicked reset state,
+  closeModal(e) {
+    if (e.target.className === 'modal-wrapper' || e.target.className === 'modal-close-button') {
+      this.props.closeModal();
+      this.setState({
+        loading: true,
+        assetData: {},
+      });
+    }
+  }
+
+
+  // Creates the modal 
+  renderModal() {
+    let modalContent;
+    if (this.state.loading) {
+      // the modal is in a loading state, render loading spinner
+      modalContent = (
+        <div className="modalLoading">
+          {/* <LoadingSpinner /> */}
+        </div>
+      );
+      // fetch the data needed
+      this.getModalContent();
+    } else {
+      const data = this.state.assetData;
+      // display the modal
+      modalContent = (
+        <div className="modal-wrapper" onClick={e => this.closeModal(e)}>
+          <div className="modal-content">
+            <span
+              role="button"
+              tabIndex={0}
+              className="modal-close-button"
+              onClick={this.closeModal}
+            >
+              &times;
+            </span>
+            <div className="modal-flex">
+              <div className="modal-image">
+                {/* Create the image modal */}
+                {this.props.modalDataObj.modalType === 'image' ?
+                  <img alt={this.props.modalDataObj.modalTitle} src={data.imageHref} /> : null}
+
+                {/* Create the video modal element */}
+                {this.props.modalDataObj.modalType === 'video' ?
+                  <video controls poster={data.vidThumb}>
+                    <source src={data.vidHref} />
+                    {data.subsHref.forEach(sub =>
+                      <track src={sub} kind="subtitles" />,
+                    )}
+                    Please use a more modern browser to play this video.
+                  </video> : null}
+
+                {/* Create the audio modal */}
+                {this.props.modalDataObj.modalType === 'audio' ?
+                  <audio controls>
+                    <source src={data.audioHref.href} type="audio/mp4" />
+                    Please use a more modern browser to play this audio.
+                  </audio> : null
+                }
+              </div>
+              {/* Render the description for the media item */}
+              <div className="modal-text">
+                <h2>{this.props.modalDataObj.modalTitle}</h2>
+                <p>{this.props.modalDataObj.modalDescription}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return modalContent;
+  }
+
+
+  render() {
+    if (this.props.isModalOpen === false) {
+      // If modal is not open do not render
+      return null;
+    }
+    return this.renderModal();
+  }
 }
 
 export default GalleryModal;
