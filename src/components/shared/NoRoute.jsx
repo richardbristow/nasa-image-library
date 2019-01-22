a946aa084558ab39de07e7ceeb06cebeec242f8c
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-01-22 20:14:07 +0000

    Added a basic NoROute back in

--- src/components/shared/NoRoute.jsx
+++ src/components/shared/NoRoute.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NoRoute = () => (
   <React.Fragment>
     Uh-oh! No page exists why dont you go back&nbsp;
-    <Link to="/search">home</Link>
+    <Link to="/">home</Link>
     .
   </React.Fragment>
 );
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import App from './components/App';
+import NoRoute from './components/shared/NoRoute';
 import getSearchParams from './utils/getSearchParams';
 
 import registerServiceWorker from './registerServiceWorker';
@@ -12,13 +13,15 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <Router>
     <Fragment>
+      <Route exact path="/" render={() => <Redirect to="/search" />} />
       <Route
-        path="/"
+        path="/search"
         render={({ location, history }) => {
           const query = getSearchParams(location);
           return <App query={query} history={history} />;
         }}
       />
+      <Route component={NoRoute} />
     </Fragment>
   </Router>, document.getElementById('root'),
 );
