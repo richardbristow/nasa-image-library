1e5b9bfdc84699f4b142e164ce57830a7dc012fc
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-04-26 15:27:47 +0100

    moved css imports to index.html
    recommended way to import these global stylings on the styled-components docs

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -3,8 +3,6 @@ import styledNormalize from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
   ${styledNormalize}
-  @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,800');
   body {
     font-family: 'Open Sans', sans-serif, serif;
   }
