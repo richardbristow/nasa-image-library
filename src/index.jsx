3d334061885a16ed6f06de41b12c949d14850120
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-05-03 15:41:43 +0100

    added ScrollToTop component

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,14 @@ import { ThemeProvider } from 'styled-components/macro';
 
 import { GlobalStyle, globalTheme } from './theme/globalStyle';
 import App from './components/App';
+import ScrollToTop from './components/shared/ScrollToTop';
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <ThemeProvider theme={globalTheme}>
     <Router>
+      <ScrollToTop />
       <GlobalStyle />
       <App />
     </Router>
